3522955cb9ae4c857cca9c8851eaa5e889e7024b
Mohsen <138538047+MohsenUSA@users.noreply.github.com>
2024-03-25 02:56:05 -0700

    Update Emojis.js

--- src/Emojis.js
+++ src/Emojis.js
@@ -13,55 +13,55 @@ const Emojis = ({ selected, onSelect }) => {
       textAlign="center"
     >
       <IconButton
-        icon={<FaSadTear style={{ fontSize: '2.5rem', color: 'red' }} />}
+        icon={<FaSadTear style={{ fontSize: '3rem', color: 'red' }} />}
         aria-label="sad"
         onClick={() => onSelect(1)}
         colorScheme="gray"
         size="xlg"
         mb={{ base: 4, md: 0 }}
-        mr={{ base: 0, md: 4 }}
+        mr={{ base: 2, md: 4 }} // Adjust margin-right here
         variant={selected === 1 ? 'solid' : 'outline'}
         borderRadius="50%"
         p="7px" // Adjust padding here
       />
       <IconButton
-        icon={<FaFrown style={{ fontSize: '2.5rem', color: 'orange' }} />}
+        icon={<FaFrown style={{ fontSize: '3rem', color: 'orange' }} />}
         aria-label="unhappy"
         onClick={() => onSelect(2)}
         colorScheme="gray"
         size="xlg"
         mb={{ base: 4, md: 0 }}
-        mr={{ base: 0, md: 4 }}
+        mr={{ base: 2, md: 4 }} // Adjust margin-right here
         variant={selected === 2 ? 'solid' : 'outline'}
         borderRadius="50%"
         p="7px" // Adjust padding here
       />
       <IconButton
-        icon={<FaMeh style={{ fontSize: '2.5rem', color: 'yellow' }} />}
+        icon={<FaMeh style={{ fontSize: '3rem', color: 'yellow' }} />}
         aria-label="neutral"
         onClick={() => onSelect(3)}
         colorScheme="gray"
         size="xlg"
         mb={{ base: 4, md: 0 }}
-        mr={{ base: 0, md: 4 }}
+        mr={{ base: 2, md: 4 }} // Adjust margin-right here
         variant={selected === 3 ? 'solid' : 'outline'}
         borderRadius="50%"
         p="7px" // Adjust padding here
       />
       <IconButton
-        icon={<FaSmile style={{ fontSize: '2.5rem', color: 'lightgreen' }} />}
+        icon={<FaSmile style={{ fontSize: '3rem', color: 'lightgreen' }} />}
         aria-label="happy"
         onClick={() => onSelect(4)}
         colorScheme="gray"
         size="xlg"
         mb={{ base: 4, md: 0 }}
-        mr={{ base: 0, md: 4 }}
+        mr={{ base: 2, md: 4 }} // Adjust margin-right here
         variant={selected === 4 ? 'solid' : 'outline'}
         borderRadius="50%"
         p="7px" // Adjust padding here
       />
       <IconButton
-        icon={<FaGrinStars style={{ fontSize: '2.5rem', color: 'green' }} />}
+        icon={<FaGrinStars style={{ fontSize: '3rem', color: 'green' }} />}
         aria-label="very-happy"
         onClick={() => onSelect(5)}
         colorScheme="gray"
